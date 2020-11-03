121c3d2e91a21e6951c2f7b3729971d6434b6585
vanshsehgal2001 <62723522+vanshsehgal2001@users.noreply.github.com>
2020-11-04 01:56:30 +0530

    Create ThirdMaxNumber.cpp

---
+++ Leetcode_Arrays_Easy/ThirdMaxNumber.cpp
@@ -0,0 +1,29 @@
+//ONE APPROACH CAN BE INTERATIVELY WHERE FIRST WE FIND LARGEST, THEN SECOND LARGEST AND THEN THE THIRD.... BUT HERE WE CAN USE MAP(WHICH ALSO TAKES O(n) TIME) BUT YES THERE IS SPACE OPTIMIZATION
+
+#include<map>
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        map<int,int> numbers;
+        for(int i=0;i<nums.size();i++){
+            numbers[nums[i]]++;
+        }
+        int count=0;
+        map<int,int>:: iterator i;
+        for(i=numbers.begin();i!=numbers.end();i++){
+            count++;
+        }
+        int left=count-3;
+        if(left<0){
+            i--;
+            return i->first;
+        }
+        for(i=numbers.begin();i!=numbers.end();i++){
+            if(left==0){
+                break;
+            }
+            left--;
+        }
+        return i->first;
+    }
+};
